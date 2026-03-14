1d7471c7151c1fe9d245e8edd13d1664d2df6b79
Tyler Ly <tylerly385@gmail.com>
2026-03-13 20:49:26 -0400

    Commented mappings in HomeController

--- src/main/java/com/example/FinalProject/controller/HomeController.java
+++ src/main/java/com/example/FinalProject/controller/HomeController.java
@@ -11,33 +11,33 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/about")
-    public String about() {
-        return "about";
-    }
-
-    @GetMapping("/products")
-    public String products() {
-        return "products";
-    }
-
-    @GetMapping("/brands")
-    public String brands() {
-        return "brands";
-    }
-
-    @GetMapping("/orders")
-    public String orders() {
-        return "orders";
-    }
-
-    @GetMapping("/products/new")
-    public String newProduct() {
-        return "product-form";
-    }
-
-    @GetMapping("/orders/new")
-    public String newOrder() {
-        return "order-form";
-    }
+//    @GetMapping("/about")
+//    public String about() {
+//        return "about";
+//    }
+//
+//    @GetMapping("/products")
+//    public String products() {
+//        return "products";
+//    }
+//
+//    @GetMapping("/brands")
+//    public String brands() {
+//        return "brands";
+//    }
+//
+//    @GetMapping("/orders")
+//    public String orders() {
+//        return "orders";
+//    }
+//
+//    @GetMapping("/products/new")
+//    public String newProduct() {
+//        return "product-form";
+//    }
+//
+//    @GetMapping("/orders/new")
+//    public String newOrder() {
+//        return "order-form";
+//    }
 }
