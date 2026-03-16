d07fdcebd050e3eaf3f036f13382d385767b9b5a
Tyler Ly <tylerly385@gmail.com>
2026-03-15 21:08:15 -0400

    Adjusted .html files and Product Class

--- src/main/java/com/example/FinalProject/entity/Product.java
+++ src/main/java/com/example/FinalProject/entity/Product.java
@@ -8,16 +8,13 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private Double price;
-
     private String description;
+    private int stockCount;
 
     @ManyToOne
     private Brand brand;
-
     @OneToOne(cascade = CascadeType.ALL)
     private Inventory inventory;
 
@@ -29,47 +26,57 @@ public class Product {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Brand getBrand() {
-        return brand;
-    }
-
-    public Inventory getInventory() {
-        return inventory;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public Double getPrice() {
+        return price;
+    }
+
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public Brand getBrand() {
+        return brand;
+    }
+
     public void setBrand(Brand brand) {
         this.brand = brand;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
     }
 
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
     }
+
+    public int getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(int stockCount) {
+        this.stockCount = stockCount;
+    }
+
+
 }
